347fe20740619a5c32d157f594cd2e622823117f
The-Henry-Ugo <93454082+The-Henry-Ugo@users.noreply.github.com>
2022-03-24 17:49:30 +0100

    Added validation for "Individual" and multi-step form for "Company"

--- js/script.js
+++ js/script.js
@@ -1,3 +1,241 @@
+//First name validation
+document.querySelector('#finm').addEventListener('blur',(event)=>{
+  let err = document.querySelector(".error-message");
+  err.innerText = "";
+  try{
+      //if field empty
+      if(event.target.validity.valueMissing)
+          throw event.target.validationMessage;
+      
+      else if(event.target.validity.patternMismatch){
+          //event.target.setCustomValidity("Enter A valid name");
+          throw "Please enter a valid name!";
+      }
+
+      else{
+          event.target.style.borderBottomColor = '#10b981';
+          err.innerText = "";
+      }
+  }
+
+  catch(messg){
+      err.innerText = messg;
+  }
+  
+});
+
+  //Last name validation
+  document.querySelector('#lamn').addEventListener('blur',(event)=>{
+    let err = document.querySelector(".error-message");
+    err.innerText = "";
+    try{
+        //if field empty
+        if(event.target.validity.valueMissing)
+            throw event.target.validationMessage;
+        
+        else if(event.target.validity.patternMismatch){
+            //event.target.setCustomValidity("Enter A valid name");
+            throw "Please enter a valid name!";
+        }
+  
+        else{
+            event.target.style.borderBottomColor = '#10b981';
+            err.innerText = "";
+        }
+    }
+  
+    catch(messg){
+        err.innerText = messg;
+    }
+    
+  });
+
+
+//to validate email
+document.querySelector('#email').addEventListener('blur',(event)=>{
+  validateEmail(event);
+});
+
+
+//function for email and confirm email validation
+function validateEmail(event){
+  let error = event.target.nextElementSibling;
+  error.innerText = "";
+  try{
+      
+      if(event.target.validity.valueMissing)
+          throw event.target.validationMessage;
+
+      else if(!event.target.validity.valid || event.target.value.slice(-4)!='.com')
+          throw "Please enter a valid email.";
+
+      else
+          event.target.style.borderBottom = '2px solid #10b981';
+       
+  }
+
+  
+  catch(messg){
+      error.innerText = messg;
+  }
+
+}
+
+
+//adding to password field
+document.querySelector("#password").addEventListener('blur',validatePassword);
+
+//password validation
+function validatePassword(event){
+  let error = event.target.parentElement.nextElementSibling;
+  error.innerText = "";
+  try{
+      console.log((event.target.value));
+      console.log();
+  if(event.target.validity.valueMissing)
+      throw event.target.validationMessage;
+
+  else if(!(/[0-9]{1,}/g).test(event.target.value)){
+      throw "There must be at least 1 digit.";
+  }
+
+  else if(event.target.value.length<8 || event.target.value.length>15){
+      
+      throw "Password must be 8 to 15 charaters.";
+  }
+
+  else{
+      // document.querySelector('[type="submit"]').removeAttribute('disabled');
+      event.target.style.borderBottom = "2px solid #10b981";
+  }
+
+  }
+  catch(messg){
+      // document.querySelector('[type="submit"]').setAttribute('disabled',"disabled");
+      error.innerText = messg;
+  }
+  console.log(error);
+}
+
+
+//confirm password validation
+document.querySelector('#confirm-password').addEventListener('blur',function(event){
+  try{
+      var error = document.querySelectorAll('.error-message')[5];
+      error.innerText = "";
+      if(event.target.validity.valueMissing)
+          throw event.target.validationMessage;
+
+      else if(document.querySelector('#password').value != event.target.value){
+          throw "Password doesn't match!";
+      }
+
+      else{
+          // document.querySelector('[type="submit"]').removeAttribute('disabled');
+          event.target.style.borderBottom = "2px solid #10b981";
+      }
+  }
+
+  catch(messg){
+      // document.querySelector('[type="submit"]').setAttribute('disabled',"disabled");
+      error.innerText = messg;
+  }
+});
+
+$(document).ready(function(){
+  $('.play-bg').on('click', function(){
+    $('.video-popup').fadeIn('slow');
+    var srchref='https://www.youtube.com/embed/bZzEZZVlfXA?autoplay=0';
+    $("#iframe").attr('src', srchref);
+    return false;
+  });
+  
+  $('.popup-bg').on('click', function(){
+    $('.video-popup').slideUp('slow');
+    return false;
+  });
+  
+   $('.close-btn').on('click', function(){
+     $('.video-popup').fadeOut('slow');
+     $("#iframe").attr('src', '');
+      return false;
+   });
+  
+});
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("fieldTab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline-flex";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Create Account";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("fieldTab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("fieldTab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" activate", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " activate";
+}
+ 
+ 
 $(function() {
   $('select').change(function() {
     if ($(this).val() == 1) {
@@ -17,68 +255,7 @@ $(function() {
     })
   })
 
-  submit = function(e) {
-        e.preventDefault();
-      const url = new URL(
-        "https://qa.rapisurv.com/api/create"
-    );
-    
-    let headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    };
-    
-    let body = {
-        "finm": "",
-        "lamn": "",
-        "phnb": "",
-        "orgn": "",
-        "unmn": "",
-        "email": "",
-        "password": "",
-        "terms": true,
-        "address": {
-            "adds": "",
-            "city": "",
-            "stat": "",
-            "poco": "",
-            "cotr": "",
-            "cotn": ""
-        },
-        "zone": 13,
-        "security": [
-            {
-                "seas": "",
-                "sequ": ""
-            },
-            {
-                "seas": "",
-                "sequ": ""
-            }
-        ]
-    }
-    
-    fetch(url, {
-        method: "POST",
-        headers,
-        body: JSON.stringify(body),
-    }).then(response => response.json())
-    .then(json => console.log(json))
-.catch(err => console.log(err));
- 
 
-
-
-    // {
-    //   "success":{"message":"Resource created"},
-    // },
-
-    // {
-    //   "message:"The action failed"
-    //   "errors":{
-    //       }
-    //   }
-};
 
 const navSlide = () => {
   const burger = document.querySelector('.burger');
@@ -264,27 +441,7 @@ $( "ul" ).on( "click", "li", function() {
 //     ]
 //   });
 
-  $(document).ready(function(){
-  
-    $('.play-bg').on('click', function(){
-      $('.video-popup').fadeIn('slow');
-      var srchref='https://www.youtube.com/embed/bZzEZZVlfXA?autoplay=0';
-      $("#iframe").attr('src', srchref);
-      return false;
-    });
-    
-    $('.popup-bg').on('click', function(){
-      $('.video-popup').slideUp('slow');
-      return false;
-    });
-    
-     $('.close-btn').on('click', function(){
-       $('.video-popup').fadeOut('slow');
-       $("#iframe").attr('src', '');
-        return false;
-     });
-    
-  });
+
 
   const company=()=>{
     document.querySelector(".individual-form").style.display = "none";
